1e8b1cad2af38abb24aabd4f46167e3ddc8041d2
Taradáj Valentin <entin044@gmail.com>
2023-06-14 18:23:27 +0200

    Service - interfész, interfész implementálás

--- Beadando/IrodaiKliensApp/Program.cs
+++ Beadando/IrodaiKliensApp/Program.cs
@@ -1,4 +1,5 @@
 using IrodaiKliensApp;
+using IrodaiKliensApp.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<IJobsService, JobService>();
 
 await builder.Build().RunAsync();
