718b1c3b0e4520ebaf0a40b0f94066622c0e20d6
Broxigar11 <71169870+Broxigar11@users.noreply.github.com>
2023-06-14 19:50:07 +0200

    JobController hozzáadása a SzerverApp projekthez

---
+++ Beadando/SzerverApp/Controllers/JobContoller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SzerverApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobContoller : ControllerBase
+    {
+        private readonly BeadandoContext _beadandoContext;
+        private readonly ILogger<JobContoller> _logger;
+
+        public JobContoller(BeadandoContext beadandoContext, ILogger<JobContoller> logger)
+        {
+            _beadandoContext = beadandoContext;
+            _logger = logger;
+        }
+
+    }
+}
