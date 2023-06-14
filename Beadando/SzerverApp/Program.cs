008e6a06a12fc69d15419840af6bcff2bfc9423e
Broxigar11 <71169870+Broxigar11@users.noreply.github.com>
2023-06-14 19:41:22 +0200

    BeadandoContext hozzáadása a Program.cs class-hoz

--- Beadando/SzerverApp/Program.cs
+++ Beadando/SzerverApp/Program.cs
@@ -1,13 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using SzerverApp;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog(
+    (_, loggerConfiguration) => loggerConfiguration
+        .MinimumLevel.Information()
+        .WriteTo.Console()
+        .WriteTo.File("log.txt", rollingInterval: RollingInterval.Day));
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BeadandoContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb"));
+        options.UseLazyLoadingProxies();
+    });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors(o => o.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +33,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
