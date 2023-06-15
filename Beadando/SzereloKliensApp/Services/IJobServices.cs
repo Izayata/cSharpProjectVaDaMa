e44bae32b7046790f812778def27e917ce0d0bf0
Izayata <moonwolf99public@gmail.com>
2023-06-15 18:02:59 +0200

    IJobServices és JobServices hozzáadása és implementálása

---
+++ Beadando/SzereloKliensApp/Services/IJobServices.cs
@@ -0,0 +1,18 @@
+﻿using Beadando.Contract;
+using System;
+
+namespace SzereloKliensApp.Services
+{
+    public interface IJobsServices
+    {
+        Task<IEnumerable<Job>?> GetAllJobAsync();
+
+        Task<Job?> GetJobsByIdAsync(int id);
+
+        Task UpdatePersonAsync(int id, Job job);
+
+        Task DeletePersonAsync(int id);
+
+        Task AddPersonAsync(Job job);
+    }
+}
