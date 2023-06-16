49324e6ae60a8947c79ebd6c49a2901f980b288a
Izayata <moonwolf99public@gmail.com>
2023-06-16 02:01:35 +0200

    Broken kliens javítása

---
+++ Beadando/UjKliens/Services/IJobServices.cs
@@ -0,0 +1,18 @@
+﻿using Beadando.Contract;
+using System;
+
+namespace UjKliens.Services
+{
+    public interface IJobServices
+    {
+        Task<IEnumerable<Job>?> GetAllJobAsync();
+
+        Task<Job?> GetJobsByIdAsync(int id);
+
+        Task UpdateJobStatusAsync(int id, JobStatus status);
+
+        Task DeletePersonAsync(int id);
+
+        Task AddPersonAsync(Job job);
+    }
+}
---
+++ Beadando/UjKliens/Services/JobServices.cs
@@ -0,0 +1,37 @@
+﻿using Beadando.Contract;
+using System;
+using System.Net.Http.Json;
+
+
+namespace UjKliens.Services
+{
+    public class JobServices : IJobServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public JobServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<IEnumerable<Job>?> GetAllJobAsync()
+        {
+            return _httpClient.GetFromJsonAsync<IEnumerable<Job>>("Job");
+        }
+
+
+        public Task<Job?> GetJobsByIdAsync(int id)
+        {
+            return _httpClient.GetFromJsonAsync<Job?>($"Job/{id}");
+        }
+
+        public async Task UpdateJobStatusAsync(int id, JobStatus status) =>
+           await _httpClient.PutAsJsonAsync($"Job/{id}/{status}", status);
+
+        public async Task DeletePersonAsync(int id) =>
+            await _httpClient.DeleteAsync($"Job/{id}");
+
+        public async Task AddPersonAsync(Job job) =>
+            await _httpClient.PostAsJsonAsync("Job", job);
+    }
+}
